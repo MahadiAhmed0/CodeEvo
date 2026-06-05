039497f4fa192ff59116157c2425b7e37d100e6e
Golam Mahadi Ahmed <mahadiahmed.2002@gmail.com>
2026-06-05 23:57:43 +0600

    Create MessagingConfig.java
    configures RabbitMQ for the Spring application by creating and registering the exchange, JSON message converter, and RabbitTemplate as Spring-managed beans

---
+++ Backend/src/main/java/com/codeevo/auth_user/config/MessagingConfig.java
@@ -0,0 +1,31 @@
+package com.codeevo.auth_user.config;
+
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MessagingConfig {
+    public static final String AUTH_EXCHANGE = "auth.exchange";
+
+    @Bean
+    public TopicExchange authExchange() {
+        return new TopicExchange(AUTH_EXCHANGE);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jsonMessageConverter());
+        return template;
+    }
+}
