bd9d6a9b5c74795a1983ebfa49e5bf3e9432df97
Golam Mahadi Ahmed <mahadiahmed.2002@gmail.com>
2026-06-05 19:21:26 +0600

    Created UserRepositoryCustomImplementation.java
    This class helps to create the methods of Refreshtoken system which adds, refresh and rotate Refreshed tokens with proper implementation with the help of userRepositoryCustom Interface.

--- Backend/src/main/java/com/codeevo/auth_user/repository/UserRepositoryCustom.java
+++ Backend/src/main/java/com/codeevo/auth_user/repository/UserRepositoryCustom.java
@@ -3,7 +3,7 @@ package com.codeevo.auth_user.repository;
 import com.codeevo.auth_user.domain.RefreshToken;
 
 public interface UserRepositoryCustom {
-    void addRefreshToken(String UserID, RefreshToken token, int maxDevices);
-    void removeRefreshToken(String UserID, String tokenHash);
-    void rotateRefreshToken(String UserID, String oldTokenHash, RefreshToken newToken, int maxDevices);
+    void addRefreshToken(String userID, RefreshToken token, int maxDevices);
+    void removeRefreshToken(String userID, String tokenHash);
+    void rotateRefreshToken(String userID, String oldTokenHash, RefreshToken newToken, int maxDevices);
 }
