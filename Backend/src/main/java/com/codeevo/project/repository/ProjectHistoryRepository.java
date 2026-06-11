b3cdfcc2584daa224e241ca29d1b6e03012ffefe
Golam Mahadi Ahmed <mahadiahmed.2002@gmail.com>
2026-06-11 18:37:12 +0600

    Update Repository
    Added Repository tags on the repository interfaces.

--- Backend/src/main/java/com/codeevo/project/repository/ProjectHistoryRepository.java
+++ Backend/src/main/java/com/codeevo/project/repository/ProjectHistoryRepository.java
@@ -4,7 +4,9 @@ import com.codeevo.project.entity.ProjectHistory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProjectHistoryRepository extends MongoRepository<ProjectHistory, String> {
     Page<ProjectHistory> findByProjectId(String ProjectId, Pageable pageable);
     void deleteByProjectId(String ProjectId);
