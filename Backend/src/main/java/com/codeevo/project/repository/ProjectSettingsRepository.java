8d5bd73adb490a39165ba14f2bc618718eedc4c8
Golam Mahadi Ahmed <mahadiahmed.2002@gmail.com>
2026-06-11 18:30:10 +0600

    Create ProjectSettingsRepository.java

---
+++ Backend/src/main/java/com/codeevo/project/repository/ProjectSettingsRepository.java
@@ -0,0 +1,11 @@
+package com.codeevo.project.repository;
+
+import com.codeevo.project.entity.ProjectSettings;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface ProjectSettingsRepository extends MongoRepository<ProjectSettings, String> {
+        Optional<ProjectSettings> findByProjectId(String projectId);
+        void deleteByProjectId(String projectId);
+}
