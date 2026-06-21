1126e75d07f420aacd92e249f5225615fe8e1b6c
Golam Mahadi Ahmed <mahadiahmed.2002@gmail.com>
2026-06-21 16:38:07 +0600

    Create CreateProjectRequest.java

---
+++ Backend/src/main/java/com/codeevo/project/dto/request/CreateProjectRequest.java
@@ -0,0 +1,17 @@
+package com.codeevo.project.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class CreateProjectRequest {
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min = 1, max = 255, message = "Name must be between 1 and 255 characters")
+    private String name;
+
+    @Size(max = 2000, message = "Description must be at most 2000 characters")
+    private String description;
+
+    private String status = "active";
+}
