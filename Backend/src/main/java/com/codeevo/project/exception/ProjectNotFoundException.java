a0fa589b66923b568cc3e7a5ab870689386a6764
Golam Mahadi Ahmed <mahadiahmed.2002@gmail.com>
2026-06-21 16:36:43 +0600

    Update ProjectNotFoundException.java

--- Backend/src/main/java/com/codeevo/project/exception/ProjectNotFoundException.java
+++ Backend/src/main/java/com/codeevo/project/exception/ProjectNotFoundException.java
@@ -1,7 +1,7 @@
 package com.codeevo.project.exception;
 
 public class ProjectNotFoundException extends RuntimeException {
-    public ProjectNotFoundException(String message) {
-        super(message);
+    public ProjectNotFoundException(String projectId) {
+        super("Project not found with id: " + projectId);
     }
 }
