1c06990eabd86389b48f759ef8f07da42a9de379
Golam Mahadi Ahmed <mahadiahmed.2002@gmail.com>
2026-06-25 00:40:49 +0600

    Create AgentType.java

---
+++ Backend/src/main/java/com/codeevo/agent/model/AgentType.java
@@ -0,0 +1,11 @@
+package com.codeevo.agent.model;
+
+/**
+ * Identifies which agent produced an event.
+ */
+public enum AgentType {
+    SUPERVISOR,
+    CHAT,
+    VISUAL_ARCHITECT,
+    CODING
+}
